f18513be5b9b5ff6948942ca5bd670046ce0019c
FlipReck <100430692+FlipReck@users.noreply.github.com>
2024-01-03 13:53:15 +0200

    Pradzia

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
